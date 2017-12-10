273cfe2d22804f2637e4a9245d7d04663801f734
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2017-12-11 03:36:32 +0800

    sram 1 read, but data_o sometimes z. fake ram

--- YaoPad.srcs/sources_1/controller/sram_controller.v
+++ YaoPad.srcs/sources_1/controller/sram_controller.v
@@ -38,7 +38,7 @@ module sram(
 	input wire                    wishbone_stb_i,
 	input wire                    wishbone_cyc_i,
 	
-	output reg[`WordBus]           wishbone_data_o,
+	output wire[`WordBus]           wishbone_data_o,
 	output reg                    wishbone_ack_o,
 
 	// Ports of 2 SRAM chips
@@ -84,6 +84,8 @@ module sram(
 	assign datain[0] = ram0_data;
 	assign datain[1] = ram1_data;
 
+	assign wishbone_data_o = datain[chipnum];
+
 	always @ (posedge clk)
 	begin
 		if (rst == `Enable)
@@ -101,8 +103,7 @@ module sram(
 					else
 						state <= `SRAM_IDLE;
 				end
-				`SRAM_READ0: state <= `SRAM_READ1;
-				`SRAM_READ1: state <= `SRAM_IDLE;
+				`SRAM_READ0: state <= `SRAM_IDLE;
 				`SRAM_WRITE0: state <= `SRAM_WRITE1;
 				`SRAM_WRITE1: state <= `SRAM_WRITE2;
 				`SRAM_WRITE2: state <= `SRAM_IDLE;
@@ -187,15 +188,19 @@ module sram(
 	begin
 		if (rst == `Enable)
 		begin
-			wishbone_data_o <= `Zero;
+			// wishbone_data_o <= `Zero;
 			wishbone_ack_o <= 0;
 		end
 		else case (state)
 			// 1-cycle ack when entering R1/W1
-			`SRAM_READ0: begin
-				wishbone_ack_o <= 1'b1;
-				wishbone_data_o <= datain[chipnum];
+			`SRAM_IDLE: begin
+			if (reading)
+				wishbone_ack_o <= 1'b1;	
 			end
+			// `SRAM_READ0: begin
+			// 	wishbone_ack_o <= 1'b1;
+			// 	// wishbone_data_o <= datain[chipnum];
+			// end
 			`SRAM_WRITE1: begin
 				wishbone_ack_o <= 1'b1;
 			end
